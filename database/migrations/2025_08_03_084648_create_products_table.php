5d73dc4cd60df823c8d4f64e94364603f1c97ae2
Siddhartha-Esunuri_mmsrx <Siddhartha.Esunuri@micromerchantsystems.com>
2025-08-03 14:25:06 +0530

    db modified

---
+++ database/migrations/2025_08_03_084648_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('category_id');
+            $table->string('name')->unique();
+            $table->decimal('price_per_kg', 10, 2);
+            $table->decimal('regular_points', 10, 2)->comment('Regular Reward Points')->default(0)->nullable();
+            $table->decimal('pre_order_points', 10, 2)->comment('Pre-Order Reward Points')->default(0)->nullable();
+            $table->decimal('product_discount', 5, 2)->default(0);
+            $table->string('product_image')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
